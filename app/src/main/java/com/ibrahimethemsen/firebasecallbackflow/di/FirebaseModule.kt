353784ef221212a5285ea1a2ce80efe1cf1abe70
ibrahim ethem <ibrahim-ethem-50@hotmail.com>
2023-01-07 16:46:48 +0300

    Scope changed

--- app/src/main/java/com/ibrahimethemsen/firebasecallbackflow/di/FirebaseModule.kt
+++ app/src/main/java/com/ibrahimethemsen/firebasecallbackflow/di/FirebaseModule.kt
@@ -4,11 +4,11 @@ import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
-@[Module InstallIn(ViewModelComponent::class)]
+@[Module InstallIn(SingletonComponent::class)]
 object FirebaseModule {
-    @[Provides ViewModelScoped]
+    @[Provides Singleton]
     fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
 }
